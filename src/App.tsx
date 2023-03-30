0c49aac1ce53eac3a3d58554fba5b4918cc7ac69
Nguyen Vu <nguyenanhvu2172000@gmail.com>
2023-03-30 12:13:09 +0700

    fix login input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { LoginScreen } from "./components/Login";
+import { MainPageScreen } from "./components/Mainpage";
 
 function App() {
+  const [token, setToken] = useState(false);
+
+  if (token) {
+    localStorage.setItem("token", JSON.stringify(token));
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      let data = JSON.parse(localStorage.getItem("token") ?? "{}");
+      setToken(data);
+    }
+  }, []);
   return (
     <Routes>
-      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/login" element={<LoginScreen setToken={setToken} />} />
+      <Route path={"/"} element={<MainPageScreen />} />
     </Routes>
   );
 }
